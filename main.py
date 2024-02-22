edd459a3750aef594dfa5dbf28fa990d8b606df9
Mark Wiesmann <wiesemann.m.o@gmail.com>
2024-02-22 21:51:37 +0100

    Beautify and add comments

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import math
 from enum import Enum
+import random
 
 Direction = Enum('Direction', ['right', 'down', 'left', 'up'])
 
-def set_next_number(x,y,dir,number, field):
+def set_next_number(x: int, y: int, dir: Direction, number: int, field: list) -> tuple[int, int, list]:
     match dir:
         case Direction.right:
             x += 1
@@ -16,7 +17,7 @@ def set_next_number(x,y,dir,number, field):
     field[y][x] = number
     return x,y,field
 
-def update_direction(x,y,dir,field):
+def update_direction(x: int, y: int, dir: Direction, field: list) -> Direction:
     match dir:
         case Direction.right:
             if field[y+1][x] == -1:
@@ -32,28 +33,36 @@ def update_direction(x,y,dir,field):
                 return Direction.right
     return dir
 
-input = 13
-digits = len(str(input))
+def main(input: int):
+    digits = len(str(input)) # We need this to pad the various numbers to form a nice grid
 
-side = math.ceil(math.sqrt(input))
+    # Calculate the side length of the square matrix
+    side = math.ceil(math.sqrt(input))
 
-output = [[-1 for x in range(side)] for y in range(side)] 
+    # Initialize the output matrix with -1
+    output = [[-1 for x in range(side)] for y in range(side)] 
 
-center = math.ceil(side/2) - 1
+    # Initialize starting position and number
+    center = math.ceil(side/2) - 1
+    x = y = center
+    current_number = 0
 
-x = y = center
-current_number = 0
-
-output[y][x] = current_number
-current_number += 1
-
-direction = Direction.right
-
-while (current_number < input):
-    x,y,output = set_next_number(x,y,direction,current_number,output)
-    direction = update_direction(x,y,direction,output)
+    output[y][x] = current_number
     current_number += 1
 
-for line in output:
-    if any(i >= 0 for i in line):
-        print(' '.join([' ' * digits if n == -1 else str(n).rjust(digits) for n in line]))
+    direction = Direction.right
+
+    while (current_number < input):
+        x,y,output = set_next_number(x,y,direction,current_number,output)
+        direction = update_direction(x,y,direction,output)
+        current_number += 1
+
+    for line in output:
+        if any(i >= 0 for i in line):
+            print(' '.join([' ' * digits if n == -1 else str(n).rjust(digits) for n in line]))
+
+for i in range(5):
+    rand = random.randint(1,100)
+    print(f'Sprial for rand: {rand}')
+    main(rand)
+    print()
